c3c99882b5592d1bb4b176536f53bac3defd9e19
mayasage <mayablacksage@proton.me>
2024-03-25 23:51:06 +0530

    princeton: UnionFindTwoTest Update - added choke test.

--- mayasage/algorithms/princeton/one/UnionFindTwoTest.java
+++ mayasage/algorithms/princeton/one/UnionFindTwoTest.java
@@ -30,5 +30,56 @@ class UnionFindTwoTest {
     assertEquals(3, uf.find(3));
     assertEquals(2, uf.find(4));
     assertEquals(1, uf.find(5));
+
+    /*
+     * This will generate a "Flat" Tree.
+     * 1 U 2 => 1 p 2
+     * 2 U 3 => 1 U 3 => 1 p 3
+     * 3 U 4 => 1 U 4 => 1 p 4
+     *
+     * It is generating a "Flat" Tree because I have written in the union
+     * condition that p will ALWAYS be the parent.
+     *
+     * Then, here we go from left to right, and left will always be the parent,
+     * and thus it generates, the "Flat" Tree.
+     *
+     * However, if we start going in reverse, then we'll see some choking.
+     */
+    uf = new UnionFindTwo(10000000);
+    for (int i = 1; i < 10000000; i += 1) {
+      uf.union(i, i + 1);
+    }
+
+    /*
+     * Let be build the tree by reversing the union operation.
+     * This will still not chock because it is growing linearly.
+     * Wait for the next loop and you'll see.
+     *
+     * This one is fast compared to UnionFindOne
+     * We're not accessing the entire array immediately for every union here.
+     * Accessing will grow linearly to N.
+     * 1U2 (search 2 elements)
+     * 2U3 (search 4 elements)
+     * As apposed to searching 10000000 every union call.
+     */
+    int n = 10000000;
+    uf = new UnionFindTwo(n);
+    for (int i = n; i > 1; i -= 1) {
+      uf.union(i - 1, i);
+    }
+
+    /*
+     * Gotta choke 'em down !
+     *
+     * We're essentially iterating through the entire set twice (once for p,
+     * once for q) for a single connected operation.
+     *
+     * This is even worse than UnionFindOne union (there we were only
+     * scanning the set once for the union operation, and zero times for
+     * connected).
+     */
+    for (int i = 0; i < n; i += 1) {
+      uf.connected(n, n);
+    }
   }
 }
