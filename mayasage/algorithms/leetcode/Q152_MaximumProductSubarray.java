6979458d5d736f5ff7d3789e450383c4542b2a79
mayasage <mayablacksage@proton.me>
2024-12-12 11:31:10 +0530

    leetcode: Q152_MaximumProductSubarray

--- mayasage/algorithms/leetcode/Q152_MaximumProductSubarray.java
+++ mayasage/algorithms/leetcode/Q152_MaximumProductSubarray.java
@@ -27,97 +27,86 @@ integer.
 package mayasage.algorithms.leetcode;
 
 public class Q152_MaximumProductSubarray {
-  /**
-   * Triple loops. Calculate every subarray.
-   *
-   * @param nums range -10 to +10
-   * @return Integer.MIN_VALUE if length == 0
-   */
-  public static int on3(int[] nums) {
-    int n = nums.length;
-    int r = Integer.MIN_VALUE;
-    for (int i = 0; i < n; i += 1) {
-      for (int j = i; j < n; j += 1) {
-        int p = 1;
-        for (int k = i; k <= j; k += 1) {
-          p *= nums[k];
+
+        public static int triple_loops(int[] nums) {
+                int max = nums[0];
+                for (int startIndex = 0; startIndex < nums.length; startIndex++) {
+                        int product = 1;
+                        for (int endIndex = startIndex; endIndex < nums.length; endIndex++) {
+                                for (int i = startIndex; i <= endIndex; i++) {
+                                        product *= nums[i];
+                                }
+                                max = Math.max(max, product);
+                                product = 1;
+                        }
+                }
+                return max;
         }
-        r = Math.max(r, p);
-      }
-    }
-    return r;
-  }
 
-  /**
-   * Double loops. O(1) calculation by caching previous result.
-   *
-   * @param nums range -10 to +10
-   * @return Integer.MIN_VALUE if length == 0
-   */
-  public static int on2(int[] nums) {
-    int n = nums.length;
-    double r = Integer.MIN_VALUE;
-    for (int i = 0; i < n; i += 1) {
-      double p = 1;
-      for (int j = i; j < n; j += 1) {
-        p *= nums[j];
-        r = Math.max(r, p);
-      }
-    }
-    return (int) r;
-  }
+        public static int double_loops(int[] nums) {
+                int max = nums[0];
+                for (int startIndex = 0; startIndex < nums.length; startIndex++) {
+                        int product = 1;
+                        for (int endIndex = startIndex; endIndex < nums.length; endIndex++) {
+                                product *= nums[endIndex];
+                                max = Math.max(max, product);
+                        }
+                }
+                return max;
+        }
 
-  /**
-   * Calculate maxSuffix and maxPrefix.
-   * If there are no -ve, both will be equal and the answer.
-   * If there are even number of -ve, both will be equal and the answer.
-   * BUT, if there are odd number of -ve, one of them will be greater and the
-   * answer.
-   *
-   * @param nums range -10 to +10
-   * @return Integer.MIN_VALUE if length == 0
-   */
-  public static int on(int[] nums) {
-    int n = nums.length;
-    double r = Integer.MIN_VALUE;
-    double c = 1;
-    for (int i = 0; i < n; i += 1) {
-      int x = nums[i];
-      c *= x;
-      r = Math.max(r, c);
-      if (x == 0) c = 1;
-    }
-    c = 1;
-    for (int i = n - 1; i >= 0; i -= 1) {
-      int x = nums[i];
-      c *= x;
-      r = Math.max(r, c);
-      if (x == 0) c = 1;
-    }
-    return (int) r;
-  }
+        public static int two_loops(int[] nums) {
+                int max = nums[0];
+                int productLeftToRight = 1;
+                for (int x : nums) {
+                        productLeftToRight *= x;
+                        max = Math.max(max, productLeftToRight);
+                        if (productLeftToRight == 0) productLeftToRight = 1;
+                }
+                int productRightToLeft = 1;
+                for (int i = nums.length - 1; i >= 0; i--) {
+                        int x = nums[i];
+                        productRightToLeft *= x;
+                        max = Math.max(max, productRightToLeft);
+                        if (productRightToLeft == 0) productRightToLeft = 1;
+                }
+                return max;
+        }
 
-  /**
-   * Intermediate answers are meaningless, but after the end of the loop, I'll
-   * have my answer.
-   *
-   * @param nums range -10 to +10
-   * @return Integer.MIN_VALUE if length == 0
-   */
-  public static int on_single_loop(int[] nums) {
-    int n = nums.length;
-    double res = Integer.MIN_VALUE;
-    double p = 1;
-    double s = 1;
-    for (int i = 0; i < n; i += 1) {
-      int l = nums[i];
-      int r = nums[n - 1 - i];
-      p *= l;
-      s *= r;
-      res = Math.max(res, Math.max(p, s));
-      if (l == 0) p = 1;
-      if (r == 0) s = 1;
-    }
-    return (int) res;
-  }
+        /**
+         * It looks like this will run worse than the 2 loops one.
+         * I read somewhere that someone between compiler, os, calculation hardware will run better if it can anticipate
+         * where it has to jump next.
+         */
+        public static int two_loops_into_one(int[] nums) {
+                int max = nums[0];
+                int productLeftToRight = 1;
+                int productRightToLeft = 1;
+                for (int i = 0; i < nums.length; i++) {
+                        int leftValue = nums[i];
+                        int rightValue = nums[nums.length - 1 - i];
+                        productLeftToRight *= leftValue;
+                        productRightToLeft *= rightValue;
+                        max = Math.max(max, productLeftToRight);
+                        max = Math.max(max, productRightToLeft);
+                        if (productRightToLeft == 0) productRightToLeft = 1;
+                        if (productLeftToRight == 0) productLeftToRight = 1;
+                }
+                return max;
+        }
+
+        public static int one_loop(int[] nums) {
+                int max = nums[0];
+                int minValueTillNow = nums[0];
+                int maxValueTillNow = nums[0];
+                for (int i = 1; i < nums.length; i++) {
+                        int x = nums[i];
+                        int minX = minValueTillNow * x;
+                        int maxX = maxValueTillNow * x;
+                        minValueTillNow = Math.min(x, Math.min(minX, maxX));
+                        maxValueTillNow = Math.max(x, Math.max(minX, maxX));
+                        max = Math.max(max, maxValueTillNow);
+                }
+                return max;
+        }
 }
---
+++ mayasage/algorithms/leetcode/Q152_MaximumProductSubarrayTest.java
@@ -0,0 +1,69 @@
+package mayasage.algorithms.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+record TestCase(int[] nums, int max) {
+}
+
+class Q152_MaximumProductSubarrayTest {
+
+        List<TestCase> getTestCases() {
+                List<TestCase> testCases = new ArrayList<>();
+                testCases.add(new TestCase(new int[]{2, 3, -2, 4}, 6));
+                testCases.add(new TestCase(new int[]{-2, 0, -1}, 0));
+                testCases.add(new TestCase(new int[]{-2}, -2));
+                testCases.add(new TestCase(new int[]{1, 2}, 2));
+                testCases.add(new TestCase(new int[]{0, 2}, 2));
+                return testCases;
+        }
+
+        @Test
+        void triple_loops() {
+                List<TestCase> testCases = getTestCases();
+                for (TestCase testCase : testCases) {
+                        int max = Q152_MaximumProductSubarray.triple_loops(testCase.nums());
+                        assertEquals(testCase.max(), max);
+                }
+        }
+
+        @Test
+        void double_loops() {
+                List<TestCase> testCases = getTestCases();
+                for (TestCase testCase : testCases) {
+                        int max = Q152_MaximumProductSubarray.double_loops(testCase.nums());
+                        assertEquals(testCase.max(), max);
+                }
+        }
+
+        @Test
+        void two_loops() {
+                List<TestCase> testCases = getTestCases();
+                for (TestCase testCase : testCases) {
+                        int max = Q152_MaximumProductSubarray.two_loops(testCase.nums());
+                        assertEquals(testCase.max(), max);
+                }
+        }
+
+        @Test
+        void two_loops_into_one() {
+                List<TestCase> testCases = getTestCases();
+                for (TestCase testCase : testCases) {
+                        int max = Q152_MaximumProductSubarray.two_loops_into_one(testCase.nums());
+                        assertEquals(testCase.max(), max);
+                }
+        }
+
+        @Test
+        void one_loop() {
+                List<TestCase> testCases = getTestCases();
+                for (TestCase testCase : testCases) {
+                        int max = Q152_MaximumProductSubarray.one_loop(testCase.nums());
+                        assertEquals(testCase.max(), max);
+                }
+        }
+}
